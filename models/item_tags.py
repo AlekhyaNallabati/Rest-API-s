c5a9bacddad721b56c84e23fe1c1701194e47912
Alekhya <alekhya.nallabati@gmail.com>
2024-11-20 22:59:50 +0530

    User Authentication with Flask-JWT-Extended

--- models/item_tags.py
+++ models/item_tags.py
@@ -1,9 +1,11 @@
 from db import db
 
 
-class ItemTags(db.schema):
+class ItemTags(db.Model):
     __tablename__ = "item_tags"
 
     id = db.Column(db.Integer, primary_key=True)
-    item_id = db.Column(db.Integer, db.ForeignKey("items_id"))
-    tag_id = db.Column(db.Integer, db.ForeignKey("tag_id"))
+    item_id = db.Column(db.Integer, db.ForeignKey("items.id"))
+    tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"))
+
+
