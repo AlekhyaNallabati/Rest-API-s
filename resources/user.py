278a97cfb9516070b1ffb10d2d3005d9c3ea4749
Alekhya <alekhya.nallabati@gmail.com>
2024-12-12 12:50:21 +0530

    Adding requirements

--- resources/user.py
+++ resources/user.py
@@ -1,3 +1,5 @@
+import os
+import requests
 from flask.views import MethodView
 from flask_smorest import Blueprint,abort
 from passlib.hash import pbkdf2_sha256
@@ -11,6 +13,19 @@ from blocklist import BLOCKLIST
 
 
 blp = Blueprint("Users", "users", description="Operations on Users")
+
+def send_simple_message(to, subject, body):
+    domain = os.getenv("MAILGUN_DOMAIN")
+    return requests.post(
+        f"https://api.mailgun.net/v3/{domain}/messages",
+        auth=("api", os.getenv("MAILGUN_API_KEY")),
+        data={
+            "from": f"Alekhya Nallabati<mailgun@{domain}>",
+            "to": [to],
+            "subject": subject,
+            "text": body,
+        },
+    )
 
 @blp.route("/register")
 class UserRegister(MethodView):
